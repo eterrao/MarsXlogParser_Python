e9867949a31e9c5f84d23171083f4ee457c46442
ericrao <eric@welove-inc.com>
2018-03-09 10:25:37 +0800

    added json parser

--- xlogParser.py
+++ xlogParser.py
@@ -8,33 +8,35 @@ xlogFilesPath = projectPath + 'xlog/'
 xlogURLFile = 'weloveXlogFiles_20180306.txt'
 
 
-def downloadFile():
+def downloadFile(url):
     response = urllib.request.urlopen(url)
     html = response.read()
-    print(html)
-
-
-# downloadFile()
+    pprint(html)
 
 
 if not os.path.exists(xlogFilesPath):
     os.mkdir(xlogFilesPath)
-
 
 # def createXlogFiles():
 #     f = open(path + 'test.txt', 'w+')
 #
 #
 # createXlogFiles()
+jsonData = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+
 
 def readFile():
     f = open(projectPath + xlogURLFile, 'r')
-    content = ""
+    content = ''
     for line in f:
         # print(line)
         content += line
-    data = json.load(f)
-    pprint(data)
+
+    text = json.loads(content)
+    # pprint(text['data']['entries'])
+    for entry in text['data']['entries']:
+        print(entry['dl_remove_attname_url'])
+        downloadFile(entry['dl_remove_attname_url'])
 
 
 readFile()
